3510a6e2ce7765d4fbd3149b5d2d317788711939
Thomas Shaddox <wediddoit@gmail.com>
2011-01-23 13:19:26 -0600

    (MIGRATION) Created the Vehicle model. Created an admin.py and registered Vehicle with it.

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Vehicle(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    owner = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return "%s %s %s owned by %s" % (self.year, self.make, self.model, self.owner)
